8846f55d6b3505d2fbe55f551c85a1dab655e267
Nave066 <jayamnaveen68@gmail.com>
2022-01-13 12:47:16 +0530

    Basic Logic Programs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FlipCoin.FlipCoinPercentage();
+            //LeapYear.findLeapYear();
+            //Powerof2.findPoweroftwo();
+            //HarmonicNumber.FindHarmonicNumber();
+            //swapNumbers.swapingNumbers();
+            //EvenorOdd.findEvenorOdd();
+            //QuotientandRemainder.findQuotientandRemainder();
+            //VowelsorConsonants.findvowelsorConsonents();
+            MaximumNumber.findMaximumNumbers();
+        }
+    }
+}
